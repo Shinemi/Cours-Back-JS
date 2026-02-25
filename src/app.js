ef74ead9173d58eb5a8f14885f12844e22ee657d
Shinemi <simon.lucas.83136@gmail.com>
2026-02-25 16:53:30 +0100

    Add CORS middleware to handle cross-origin requests

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import quoteRoutes from './routes/quoteRoutes.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const app =  express()
+
+//permet de gérer les requetes cross-origin (quand le front et le back sont sur des ports différents)
+app.use(cors())
 
 //body parser 
 app.use(express.json())
